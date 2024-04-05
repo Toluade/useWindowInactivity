06cf5a4d93df58ab5abae192e3e962557d315b96
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2024-04-05 20:31:08 +0100

    created index.ts

---
+++ index.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useWindowInactivity = (delay: number = 5) => {
+  const [inactive, setInactive] = useState(false);
+  const millisenconds = delay * 1000;
+
+  const events = ["mousemove", "touchstart"];
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    events.forEach((event) =>
+      window.addEventListener(event, () => {
+        clearTimeout(timeout);
+        setInactive(false);
+        timeout = setTimeout(() => {
+          setInactive(true);
+        }, millisenconds);
+      })
+    );
+
+    return () =>
+      events.forEach((event) =>
+        window.removeEventListener(event, () => {
+          clearTimeout(timeout);
+          setInactive(false);
+          timeout = setTimeout(() => {
+            setInactive(true);
+          }, millisenconds);
+        })
+      );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return inactive;
+};
+
+export default useWindowInactivity;
